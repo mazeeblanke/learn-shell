43bfdfce654188d90499b7ea20aacfa785f7a481
mazino <ewomaukah@yahoo.com>
2020-07-25 18:23:21 +0100

    Create simple addtion example

---
+++ src/operators/addtion.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# When creating/initializing a variable, remember to not add space between the '=' and the value/variable name
+number1=100
+number2=20
+
+# Add two numbers
+echo "#### Addtion section ####"
+echo $((3+7))
+echo $(( 3+7 ))
+expr 3 + 6 # doea not need 'echo' command to print stuffs out to the terminal
+expr $number1 + $number2 # when performing an operation add the $ sign, when assigning remove it
+echo $(( number1 + number2 )) # without the echo command, the expression will be processed as a command
+echo "#########################"
